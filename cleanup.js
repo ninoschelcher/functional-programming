19abd7c7c08a5e9e3de2bec712dd980ac5b10629
Nino Schelcher <onin1998@gmail.com>
2020-10-22 13:34:57 +0200

    remade format function

--- cleanup.js
+++ cleanup.js
@@ -1,3 +1,82 @@
+const surveyData = data;
+let surveyQuestion = "lievelingskleur";
+
+const getSpecificAnswer = (surveyData, surveyQuestion) => {
+  let specificAnswers = [];
+  surveyData.map(surveyData => specificAnswers.push(surveyData[surveyQuestion]))
+  console.log(specificAnswers);
+  removeFaultyAnswers(specificAnswers);
+}
+
+const removeFaultyAnswers = (specificAnswers) => {
+  const faultyFilter = specificAnswers.filter(answer => {
+    return answer !== '' && answer !== '/' && answer !== '0';
+  });
+  formatAnswers(faultyFilter);
+}
+
+const formatAnswers = (correctedAnswers) => {
+  let rightAnswers = [];
+  let wrongAnswers = [];
+
+  const allWrittenColours = {
+    legergroen: '#54552B',
+    staal: '#5D6565',
+    grijs: '#b8b8b8',
+    zwart: '#000000',
+    rood: '#FF0101',
+    geel: '#F7F700'
+  }
+
+  const writtenColours = Object.keys(allWrittenColours)
+
+  correctedAnswers.forEach(answer => {
+    answer = answer.split('.').join("");
+    answer = answer.split(" ")[0];
+    answer = answer.toLowerCase();
+
+    if (writtenColours.includes(answer)) {
+    wrongAnswers.push(allWrittenColours[answer]);
+    } else {
+      if(answer.charAt(0) != '#') {
+        answer = '#' + answer;
+      }
+      if (answer.length > 4 && answer.length < 7) {
+        answer = answer.slice(0, 4);
+      }
+      rightAnswers.push(answer);
+    }
+  });
+
+  let cleanedData = wrongAnswers.concat(rightAnswers).sort();
+  showCircles(cleanedData);
+}
+
+const showCircles = (formattedAnswers) => {
+  formattedAnswers.forEach(answer => {
+    const block = document.createElement('div');
+    const favouriteColour = document.createElement('div');
+    const colourCode = document.createElement('p');
+
+    block.className = 'circle';
+    colourCode.innerText = answer;
+    favouriteColour.className = 'favouritecolour'
+    favouriteColour.style.backgroundColor = answer;
+    if(answer === '#ffffff') {
+      favouriteColour.style.border = "3px solid black";
+    }
+
+    document.querySelector('.overview').appendChild(block);
+    block.appendChild(colourCode);
+    block.appendChild(favouriteColour);
+  })
+}
+
+let cleanData = getSpecificAnswer(surveyData, surveyQuestion);
+
+
+
+
 // fetch('survey_data.json')
 //   .then(response => {
 //     return response.json();
@@ -47,78 +126,3 @@
 
   // surveyData.map(entry => specificAnswers.push(entry[surveyQuestion]));
   // removeFaultyAnswers(specificAnswers);
-
-
-
-const surveyData = data;
-let surveyQuestion = "lievelingskleur";
-
-const getSpecificAnswer = (surveyData, surveyQuestion) => {
-  let specificAnswers = [];
-  surveyData.map(surveyData => specificAnswers.push(surveyData[surveyQuestion]))
-  removeFaultyAnswers(specificAnswers);
-}
-
-const removeFaultyAnswers = (specificAnswers) => {
-  const faultyFilter = specificAnswers.filter(answer => {
-    return answer !== '' && answer !== '/' && answer !== '0';
-  });
-  formatAnswers(faultyFilter);
-}
-
-const formatAnswers = (correctedAnswers) => {
-  let rightAnswers = [];
-  let wrongAnswers = [];
-  
-  const allWrittenColours = {
-    legergroen: '#54552B',
-    staal: '#5D6565',
-    grijs: '#b8b8b8',
-    zwart: '#000000',
-    rood: '#FF0101',
-    geel: '#F7F700'
-  }
-
-  const writtenColours = Object.keys(allWrittenColours)
-
-  correctedAnswers.forEach(answer => {
-    answer = answer.split('.').join("");
-    answer = answer.split(" ")[0];
-    answer = answer.toLowerCase();
-    if(answer.charAt(0) == '#') {
-      rightAnswers.push(answer);
-    } else {
-      if(writtenColours.includes(answer)) {
-        wrongAnswers.push(allWrittenColours[answer]);
-      } else {
-        answer = '#' + answer;
-        rightAnswers.push(answer);
-      }
-    }
-  });
-
-  let cleanedData = wrongAnswers.concat(rightAnswers).sort();
-  showCircles(cleanedData);
-}
-
-const showCircles = (formattedAnswers) => {
-  formattedAnswers.forEach(answer => {
-    const block = document.createElement('div');
-    const favouriteColour = document.createElement('div');
-    const colourCode = document.createElement('p');
-
-    block.className = 'circle';
-    colourCode.innerText = answer;
-    favouriteColour.className = 'favouritecolour'
-    favouriteColour.style.backgroundColor = answer;
-    if(answer === '#ffffff') {
-      favouriteColour.style.border = "3px solid black";
-    }
-
-    document.querySelector('.overview').appendChild(block);
-    block.appendChild(colourCode);
-    block.appendChild(favouriteColour);
-  })
-}
-
-let cleanData = getSpecificAnswer(surveyData, surveyQuestion);
